c82a69c64b99c235f01731d008384b832c43bc17
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-03-14 09:38:00 +1300

    Added basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="w-full h-full bg-red-500">
-    <div class="container">s</div>
+  <div id="container" class="w-full h-full light-gray flex items-center justify-center">
+    <div id="panel" class="h-96 bg-white rounded-lg flex justify-center">
+      <div id="qr-code" class="qr w-60 h-60 bg-contain rounded-lg m-4"></div>
+      <h1></h1>
+    </div>
   </div>
 </template>
 
