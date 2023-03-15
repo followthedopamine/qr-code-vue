c20a5097c1252d308d94bcfc62089ebe36c62b2e
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-03-16 12:22:14 +1300

    Finished design

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div id="container" class="w-full h-full light-gray flex items-center justify-center">
-    <div id="panel" class="h-96 bg-white rounded-lg flex justify-center">
-      <div id="qr-code" class="qr w-60 h-60 bg-contain rounded-lg m-4"></div>
-      <h1>Improve your front-end skills by building projects</h1>
+  <div id="container" class="w-full h-full bg-light-gray flex items-center justify-center">
+    <div id="panel" class="bg-white rounded-[20px] flex flex-col items-center">
+      <div id="qr-code" class="qr w-[290px] h-[290px] bg-cover rounded-lg m-4"></div>
+      <div id="text" class="w-[260px] mb-6">
+        <h1 class="mb-4 mt-2 text-[22px] font-bold text-center leading-7 text-dark-blue">
+          Improve your front-end skills by building projects
+        </h1>
+        <p class="mb-4 text-[15px] text-center text-light-blue leading-5">
+          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+        </p>
+      </div>
     </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    fontFamily: {
+      sans: ['Outfit', 'ui-sans-serif', 'system-ui']
+    },
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      'light-gray': 'hsl(212, 45%, 89%)',
+      'light-blue': 'hsl(220, 15%, 55%)',
+      'dark-blue': 'hsl(218, 44%, 22%)'
+    }
   },
   plugins: []
 }
