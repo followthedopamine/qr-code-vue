a19a1a1c57215360b66c9bf4e17b3cd5402d9011
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-03-15 22:30:34 +1300

    Added heading

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <div id="container" class="w-full h-full light-gray flex items-center justify-center">
     <div id="panel" class="h-96 bg-white rounded-lg flex justify-center">
       <div id="qr-code" class="qr w-60 h-60 bg-contain rounded-lg m-4"></div>
-      <h1></h1>
+      <h1>Improve your front-end skills by building projects</h1>
     </div>
   </div>
 </template>
